349f6c10aa7b77d59884b39d5a33fcd180123700
Jimmy <jimmo82@gmail.com>
2023-04-04 16:14:00 -0600

    Add language tags and gist name

--- src/Gist.tsx
+++ src/Gist.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
-import { Avatar, Box, Link, Typography } from "@mui/material";
+import { Avatar, Box, Chip, Link, Typography } from "@mui/material";
 import { makeGetRequest } from "./request";
 
 function Gist({ gist }) {
-  const { owner, description, forks_url } = gist;
+  const { files, description, forks_url } = gist;
 
   const [forks, setForks] = React.useState([]);
+  const name = Object.keys(files)[0] || "No name";
 
   React.useEffect(() => {
     makeGetRequest(forks_url)
@@ -16,17 +17,29 @@ function Gist({ gist }) {
       .catch((error) => console.log(error));
   }, [forks_url]);
 
+  const getTagsFromFiles = (files) => {
+    const fileTypes = Object.keys(files).map((key) =>
+      files[key].language ? files[key].language.toLowerCase() : "unknown"
+    );
+
+    const uniqueFileTypes = [...new Set(fileTypes)];
+
+    return uniqueFileTypes.map((fileType) => (
+      <Chip key={fileType} label={fileType} size="small" />
+    ));
+  };
+
   return (
     <Box sx={{ display: "flex", alignItems: "center", mb: 1 }}>
-      {/* <Avatar sx={{ mr: 1 }} src={owner.avatar_url} alt={owner.login} /> */}
-      {/* <Box>
+      <Box>
         <Typography variant="subtitle2" gutterBottom>
-          {owner.login}
+          {name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
           {description}
         </Typography>
-      </Box> */}
+        <Box mt={1}>{getTagsFromFiles(gist.files)}</Box>
+      </Box>
       <Box sx={{ ml: "auto", display: "flex", alignItems: "center" }}>
         {forks.map((fork: any) => (
           <Box key={fork.id} sx={{ ml: 1 }}>
